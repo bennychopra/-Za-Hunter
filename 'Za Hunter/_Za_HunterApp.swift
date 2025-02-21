79afb72f95e0712cb0b38c7acbd4d42bb2e2092c
Benny Chopra <bchopra@bsd220.org>
2025-02-21 08:42:24 -0600

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Benny Chopra on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
